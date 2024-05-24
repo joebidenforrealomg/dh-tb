33662068f636b30e25140f13aeaee1042c2a607f
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-05-24 12:45:43 -0700

    update | new website theme & app opening improvements

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/* 
-NOTE TO ANYONE READING THE CODE:
-I know most of this code sucks, I made it close to a year ago.
-I'm currently trying to convert this garbage code into something reasonable. In the meantime, I'm doing goofy things to ensure this doesn't break with constant updates.
-*/
-
-var hubWindow = null;
-
 const appsDiv = document.getElementById("apps");
 const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
@@ -20,83 +12,117 @@ const apps = ["1v1.LOL", "2048", "Aquapark", "Angry Birds", "Bad Piggies",
     "SuperMario 64", "SoobwaySurfers", "Tetris", "Worlds Hardest Game_1", "Worlds Hardest Game_2", "Wordle", "yohoho.io"];
 apps.sort();
 
-function openSite(url, title, icon) {
-    var blank = window.open();
-    var link = blank.document.createElement('link');
-    var style = blank.document.createElement('style');
-    var closeButton = blank.document.createElement('p');
-    var iframe = blank.document.createElement('iframe');
-    link.rel = "shortcut icon";
-    link.href = icon || "";
-    blank.document.title = title || "New Tab";
-    closeButton.innerText = "BACK";
-    style.innerHTML = `body { width: 100vw;height: 100vh;margin: 0; background: black; } iframe { width: 100vw;height: 100vh;border: none;outline: none;margin: 0;} p { cursor: pointer;font-family: monospace;position: fixed;z-index: 2;padding: 8px;left: 0;transform: translateX(-50%);transition: 0.2s ease;opacity: 0.5;background: black;border: 2px solid lime;color: lime;} p:hover { left: 8px;transform: translateX(0);opacity: 1;}`;
-    closeButton.addEventListener('click', function() {
-        iframe.src = "https://joebidenrealomg.github.io/da-hub/index.html?iframe=true";
-        if (hubWindow) {
-            hubWindow.close();
-        }
-    });
-    iframe.src = `${url}`;
-    blank.document.head.appendChild(style);
-    blank.document.head.appendChild(link);
-    blank.document.body.appendChild(iframe);
-    if (!url.includes("/da-hub/")) {
-        blank.document.body.appendChild(closeButton);
-    } else {
-        hubWindow = blank;
+function openWindow(url, title, icon) {
+  var blank = window.open();
+  var link = blank.document.createElement('link');
+  var style = blank.document.createElement('style');
+  var iframe = blank.document.createElement('iframe');
+  
+  link.rel = "shortcut icon";
+  link.href = icon || "";
+  style.innerHTML = `body { width: 100vw;height: 100vh;margin: 0; background: black; } iframe { width: 100vw;height: 100vh;border: none;outline: none;margin: 0;} p { cursor: pointer;font-family: monospace;position: fixed;z-index: 2;padding: 8px;left: 0;transform: translateX(-50%);transition: 0.2s ease;opacity: 0.5;background: black;border: 2px solid lime;color: lime;} p:hover { left: 8px;transform: translateX(0);opacity: 1;}`;
+  iframe.src = `${url}`;
+  blank.document.title = title || "New Tab";
+  
+  blank.document.head.appendChild(style);
+  blank.document.head.appendChild(link);
+  blank.document.body.appendChild(iframe);
+  
+  window.location.replace("https://google.com");
+}
+
+function openSite(url) {
+  if (document.getElementById("appDiv")) {
+    document.getElementById("appDiv").remove();
+  }
+  
+  var appDiv = document.createElement('div');
+  var closeButton = document.createElement('p');
+  var iframe = document.createElement('iframe');
+  
+  appDiv.id = "appDiv";
+  closeButton.innerText = "BACK";
+  closeButton.className = "appClose";
+  iframe.src = url;
+  iframe.className = "appIframe";
+  
+  closeButton.addEventListener('click', function() {
+    if (confirm("Are you sure you want to close this app?") === true) {
+      appDiv.remove();
     }
+  });
+
+  appDiv.appendChild(iframe);
+  appDiv.appendChild(closeButton);
+  document.body.appendChild(appDiv);
 }
 
 function searchApp(name) {
-    var foundApps = [];
-    var results = 0;
-    appsDiv.innerHTML = "";
-    if (name != undefined) {
-        apps.forEach(function (app, i) {
-            if (app.toLowerCase().includes(name.toLowerCase())) {
-                foundApps.push(app);
-                results++;
-            }
-        });
-        foundApps.sort();
-        foundApps.forEach(createApps);
-        if (results == 1) {
-            resultsText.innerHTML = `${results} result for '${name}'`;
-        } else {
-            resultsText.innerHTML = `${results} results for '${name}'`;
-        }
+  var foundApps = [];
+  var results = 0;
+  appsDiv.innerHTML = "";
+  if (name !== undefined) {
+    apps.forEach(function (app, i) {
+      if (app.toLowerCase().includes(name.toLowerCase())) {
+        foundApps.push(app);
+        results++;
+      }
+    });
+    foundApps.sort();
+    foundApps.forEach(createApps);
+    if (results == 1) {
+        resultsText.innerHTML = `${results} result for '${name}'`;
     } else {
-        apps.forEach(createApps);
-        resultsText.innerHTML = "";
+        resultsText.innerHTML = `${results} results for '${name}'`;
     }
+  } else {
+    apps.forEach(createApps);
+    resultsText.innerHTML = "";
+  }
 }
 
 function createApp(name, hint, url) {
-    const b = document.createElement("button");
-    b.innerHTML = name;
-    b.title = hint;
-    b.onclick = function () {
-        openSite(`${url}`);
-    };
-    document.getElementById("apps").appendChild(b);
+  const b = document.createElement("button");
+  const p = document.createElement("p");
+  const img = document.createElement("img");
+  p.innerText = name;
+  b.title = hint;
+  b.onclick = function () {
+    openSite(`${url}`);
+  };
+  b.appendChild(p);
+  b.appendChild(img);
+  document.getElementById("apps").appendChild(b);
+  
+  img.onerror = function() {
+    if (img.src.includes(".png")) {
+      img.src = `${url}/../thumbnail.jpg`;
+    } else if (img.src.includes(".jpg")) {
+      img.src = `${url}/../thumbnail.jpeg`;
+    } else if (img.src.includes(".jpeg")) {
+      img.src = `${url}/../thumbnail.webp`;
+    } else {
+      img.src = "https://img.freepik.com/premium-photo/digital-black-green-squares_161488-652.jpg";
+    }
+  };
+  img.src = `${url}/../thumbnail.png`;
 }
 
 function createApps(app, i) {
-    if (app.includes("_")) {
-        var a = app.split("_");
-        createApp(a[0] + " " + a[1], "Play " + a[0] + " " + a[1], "https://joebidenrealomg.github.io/da-hub/apps/" + a[0] + "/" + a[1] + "/index.html");
-    } else {
-        createApp(app, "Play " + app, "https://joebidenrealomg.github.io/da-hub/apps/" + app + "/index.html");
-    }
+  if (app.includes("_")) {
+    var a = app.split("_");
+    createApp(a[0] + " " + a[1], "Play " + a[0] + " " + a[1], "https://joebidenrealomg.github.io/da-hub/apps/" + a[0] + "/" + a[1] + "/index.html");
+  } else {
+    createApp(app, "Play " + app, "https://joebidenrealomg.github.io/da-hub/apps/" + app + "/index.html");
+  }
 }
 
 function handleSearch(e) {
-    e.preventDefault();
-    if (searchInput.value != "") {
-        searchApp(searchInput.value);
-        searchInput.value = "";
-    }
+  e.preventDefault();
+  if (searchInput.value !== "") {
+    searchApp(searchInput.value);
+    searchInput.value = "";
+  }
 }
 
 searchForm.addEventListener("submit", handleSearch);
@@ -104,30 +130,30 @@ clear.addEventListener("click", function () { searchApp() });
 apps.forEach(createApps);
 
 function InIframe() {
-    try {
-        return window.self !== window.top;
-    } catch (e) {
-        return true;
-    }
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
 }
 
 function whenNotIframe() {
-    let params = new URL(document.location).searchParams;
-    let iframe = params.get("iframe");
-    if (iframe != "true" || iframe == null || iframe == undefined) {
-        document.body.innerHTML = "";
+  let params = new URL(document.location).searchParams;
+  let iframe = params.get("iframe");
+  if (iframe != "true" || iframe == null || iframe == undefined) {
+    document.body.innerHTML = "";
 
-        const apps = document.createElement('div');
-        apps.className = "apps";
-        apps.id = "apps";
+    const apps = document.createElement('div');
+    apps.className = "apps";
+    apps.id = "apps";
 
-        const app = document.createElement('button');
-        app.onclick = function() {
-            openSite("https://joebidenrealomg.github.io/da-hub/index.html?iframe=true");
-            window.location.replace("https://google.com");
-        };
-        app.title = "Click to open";
-        app.innerText = "Open";
+    const app = document.createElement('button');
+    app.onclick = function() {
+        openWindow("https://joebidenrealomg.github.io/da-hub/index.html?iframe=true");
+        window.location.replace("https://google.com");
+    };
+    app.title = "Click to open";
+    app.innerText = "Open";
 
         apps.appendChild(app);
         document.body.appendChild(apps);
