f68211fa3ef385fca29ab1348ed293938e5457cf
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-05-30 10:28:27 -0700

    update | improvements

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
+const altCSS = document.createElement("style");
 const appsDiv = document.getElementById("apps");
 const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 const resultsText = document.getElementById("results");
 const clear = document.getElementById("clear");
 const apps = ["1v1.LOL", "2048", "Aquapark", "Angry Birds", "Bad Piggies",
-    "Bitlife", "Blackjack", "BloonsTD", "Bloxorz", /*"ButterDog Chat",*/ "CookieClicker", "CrossyRoad",
+    "Bitlife", "Blackjack", "BloonsTD", "Bloxorz", "ButterDog Chat", "CookieClicker", "CrossyRoad",
     "DonkeyKong", "DOOM", "DuckHunt", "Ducklife_1", "Ducklife_2", "Ducklife_3", "ElectricMan 2",
-    "FNAF_1", "FNAF_2", "FNAF_3", "FNAF_4", "FNAW", "GoogleDino", "GoogleSnake", "Hell Roads", "Impossible Quiz", "Jacksmith",
+    "FNAF_1", "FNAF_2", "FNAF_3", "FNAF_4", "FNAW", "GoogleDino", "GoogleSnake", "Impossible Quiz", "Jacksmith",
     "Learn 2 Fly", "Minecraft_1.8.8", "MotoX3M", "MotoX3M 2", "Minesweeper", "Pacman", "PappaPizza", "wolfen",
     "Pong", "RetroBowl", "Run_1", "Run_3", "Robux Generator Clicker", "RiddleSchool", "Slope_1", "Slope_Ass", "StickmanHook", "SuperMario 63",
     "SuperMario 64", "SoobwaySurfers", "Tetris", "Worlds Hardest Game_1", "Worlds Hardest Game_2", "Wordle", "yohoho.io"];
 apps.sort();
+
+document.head.appendChild(altCSS);
+
+function elementVisible(elem) {
+  return !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
+}
 
 function openWindow(url, title, icon) {
   var blank = window.open();
@@ -60,8 +67,12 @@ function openSite(url) {
 function searchApp(name) {
   var foundApps = [];
   var results = 0;
-  appsDiv.innerHTML = "";
-  if (name !== undefined) {
+  // appsDiv.innerHTML = "";
+  if (name !== "") {
+    altCSS.innerHTML = `.appsButton { display: none; animation: none; }`;
+    document.querySelectorAll(".appsButton").forEach((app) => {
+      app.style = "";
+    });
     apps.forEach(function (app, i) {
       if (app.toLowerCase().replaceAll("_", " ").includes(name.toLowerCase())) {
         foundApps.push(app);
@@ -70,59 +81,75 @@ function searchApp(name) {
     });
     foundApps.sort();
     foundApps.forEach(createApps);
-    if (results == 1) {
+    if (name !== "") {
+      if (results == 1) {
         resultsText.innerHTML = `${results} result for '${name}'`;
-    } else {
+      } else {
         resultsText.innerHTML = `${results} results for '${name}'`;
+      }
+    } else {
+      resultsText.innerHTML = "";
     }
   } else {
+    altCSS.innerHTML = `.appsButton { display: block; animation: none; }`;
     apps.forEach(createApps);
     resultsText.innerHTML = "";
   }
 }
 
 function createApp(name, hint, url) {
-  const b = document.createElement("button");
-  const p = document.createElement("p");
-  const img = document.createElement("img");
-  p.innerText = name;
-  b.title = hint;
-  b.onclick = function () {
-    openSite(`${url}`);
-  };
-  b.appendChild(p);
-  b.appendChild(img);
-  document.getElementById("apps").appendChild(b);
-  
-  img.onerror = function() {
-    if (img.src.includes(".png")) {
-      img.src = `${url}/../thumbnail.jpg`;
-    } else if (img.src.includes(".jpg")) {
-      img.src = `${url}/../thumbnail.jpeg`;
-    } else if (img.src.includes(".jpeg")) {
-      img.src = `${url}/../thumbnail.webp`;
-    } else {
-      img.src = "https://img.freepik.com/premium-photo/digital-black-green-squares_161488-652.jpg";
-    }
-  };
-  img.src = `${url}/../thumbnail.png`;
+  if (!document.getElementById(`appButton${name}`)) {
+    const b = document.createElement("button");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+    b.id = `appButton${name}`;
+    b.className = "appsButton";
+    p.innerText = name;
+    b.title = hint;
+    b.onclick = function () {
+      openSite(`${url}`);
+    };
+    b.appendChild(p);
+    b.appendChild(img);
+    document.getElementById("apps").appendChild(b);
+    
+    img.onload = function() {
+      img.style.opacity = 1;
+    };
+    
+    img.onerror = function() {
+      if (img.src.includes(".png")) {
+        img.src = `${url}/../thumbnail.jpg`;
+      } else if (img.src.includes(".jpg")) {
+        img.src = `${url}/../thumbnail.jpeg`;
+      } else if (img.src.includes(".jpeg")) {
+        img.src = `${url}/../thumbnail.webp`;
+      } else {
+        img.src = "https://img.freepik.com/premium-photo/digital-black-green-squares_161488-652.jpg";
+      }
+    };
+    img.src = `${url}/../thumbnail.png`;
+  }
 }
 
 function createApps(app, i) {
-  if (app.includes("_")) {
-    var a = app.split("_");
-    createApp(a[0] + " " + a[1], "Play " + a[0] + " " + a[1], "https://joebidenrealomg.github.io/da-hub/apps/" + a[0] + "/" + a[1] + "/index.html");
+  if (!document.getElementById(`appButton${app}`)) {
+    if (app.includes("_")) {
+      var a = app.split("_");
+      createApp(a[0] + " " + a[1], "Play " + a[0] + " " + a[1], "https://joebidenrealomg.github.io/da-hub/apps/" + a[0] + "/" + a[1] + "/index.html");
+    } else {
+      createApp(app, "Play " + app, "https://joebidenrealomg.github.io/da-hub/apps/" + app + "/index.html", i);
+    }
   } else {
-    createApp(app, "Play " + app, "https://joebidenrealomg.github.io/da-hub/apps/" + app + "/index.html");
+    document.getElementById(`appButton${app}`).style.animation = "";
+    document.getElementById(`appButton${app}`).style.display = "block";
+    document.getElementById(`appButton${app}`).style.animation = "fadeIn 0.2s ease";
   }
 }
 
 function handleSearch(e) {
   e.preventDefault();
-  if (searchInput.value !== "") {
-    searchApp(searchInput.value);
-    // searchInput.value = "";
-  }
+  searchApp(searchInput.value);
 }
 
 searchForm.addEventListener("submit", handleSearch);
@@ -144,7 +171,7 @@ function InIframe() {
 function whenNotIframe() {
   let params = new URL(document.location).searchParams;
   let iframe = params.get("iframe");
-  if (iframe != "true" || iframe == null || iframe == undefined) {
+  if (iframe !== "true" || iframe === null || iframe === undefined) {
     document.body.innerHTML = "";
 
     const apps = document.createElement('div');
@@ -153,15 +180,15 @@ function whenNotIframe() {
 
     const app = document.createElement('button');
     app.onclick = function() {
-        openWindow("https://joebidenrealomg.github.io/da-hub/index.html?iframe=true");
-        window.location.replace("https://google.com");
+      openWindow("https://joebidenrealomg.github.io/da-hub/index.html?iframe=true");
+      window.location.replace("https://google.com");
     };
     app.title = "Click to open";
     app.innerText = "Open";
 
-        apps.appendChild(app);
-        document.body.appendChild(apps);
-    }
+    apps.appendChild(app);
+    document.body.appendChild(apps);
+  }
 }
 
 if (!InIframe()) {
