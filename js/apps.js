e6f02ca7224c175041cd684e781c6d6aab048132
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-10-01 02:05:33 +0000

    update | sept. 30 update

---
+++ js/apps.js
@@ -0,0 +1,308 @@
+const apps = [
+  {
+    Name:"1v1.LOL",
+    Thumbnail:"thumbnail.jpg",
+  },
+  {
+    Name:"2048",
+    Thumbnail:"thumbnail.png",
+  },
+  {
+    Name:"Among Us",
+    Folder:"AmongUs",
+    Hidden:true,
+  },
+  {
+    Name:"Angry Birds",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Aquapark",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Bad Piggies",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Basketball Stars",
+    Thumbnail:"thumbnail.jpg",
+    Added: new Date("September 30, 2024"),
+  },
+  {
+    Name:"Bitlife",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Blackjack",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Bloons TD",
+    Folder:"BloonsTD",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Bloxorz",
+    Thumbnail:"thumbnail.jpeg"
+  },
+  {
+    Name:"Chess",
+    Thumbnail:"thumbnail.png",
+    Added: new Date("September 30, 2024"),
+  },
+  {
+    Name:"Cookie Clicker",
+    Folder:"CookieClicker",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Crossy Road",
+    Folder:"CrossyRoad",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Cubefield",
+    Thumbnail:"thumbnail.png",
+    Hidden:true
+  },
+  {
+    Name:"Donkey Kong",
+    Folder:"DonkeyKong",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"DOOM",
+    Thumbnail:"thumbnail.jpeg"
+  },
+  {
+    Name:"Duck Hunt",
+    Folder:"DuckHunt",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Ducklife 1",
+    Folder:"Ducklife/1/",
+    Thumbnail:"thumbnail.jpeg"
+  },
+  {
+    Name:"Ducklife 2",
+    Folder:"Ducklife/2/",
+    Thumbnail:"thumbnail.jpeg"
+  },
+  {
+    Name:"Ducklife 3",
+    Folder:"Ducklife/3/",
+    Thumbnail:"thumbnail.jpeg"
+  },
+  {
+    Name:"Electric Man 2",
+    Folder:"ElectricMan 2",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"FNAF 1",
+    Folder:"FNAF/1/",
+    Thumbnail:"thumbnail.jpeg"
+  },
+  {
+    Name:"FNAF 2",
+    Folder:"FNAF/2/",
+    Thumbnail:"thumbnail.jpeg",
+    Broken:true,
+  },
+  {
+    Name:"FNAF 3",
+    Folder:"FNAF/3/",
+    Thumbnail:"thumbnail.webp",
+    Broken:true,
+  },
+  {
+    Name:"FNAF 4",
+    Folder:"FNAF/4/",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"FNAW",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Fruit Ninja",
+    Folder:"FruitNinja",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Google Baseball",
+    Hidden:true,
+  },
+  {
+    Name:"Google Dino",
+    Hidden:true,
+  },
+  {
+    Name:"Google Snake",
+    Folder:"Google Snake",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Impossible Quiz",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Jacksmith",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Learn 2 Fly",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Mario Kart DS",
+    Hidden:true,
+  },
+  {
+    Name:"Minecraft 1.8",
+    Folder:"Minecraft/1.8.8/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Minesweeper",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"MotoX3M",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"MotoX3M 2",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Pacman",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Papa's Pizzeria",
+    Folder:"PapaPizza",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Pong",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Retro Bowl",
+    Folder:"RetroBowl",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Riddle School 1",
+    Folder:"RiddleSchool/1/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Riddle School 2",
+    Folder:"RiddleSchool/2/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Riddle School 3",
+    Folder:"RiddleSchool/3/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Robux Generator Clicker",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Run 1",
+    Folder:"Run/1/",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Run 2",
+    Folder:"Run/2/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Run 3",
+    Folder:"Run/3/",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Slope",
+    Folder:"Slope/1/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Slope Ass",
+    Folder:"Slope/Ass/",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Soobway Surfers",
+    Folder:"SoobwaySurfers",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Stickman Hook",
+    Folder:"StickmanHook/",
+    Thumbnail:"thumbnail.png",
+    Fixed:true
+  },
+  {
+    Name:"Super Mario 63",
+    Folder:"SuperMario 63/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Super Mario 64",
+    Folder:"SuperMario 64/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Tetris",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Ultimate Flash Sonic",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Vex 3",
+    Folder:"Vex/3/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"Wolfenstein 3D",
+    Folder:"wolfen/",
+    Thumbnail:"thumbnail.png"
+  },
+  {
+    Name:"Wordle",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"World's Hardest Game 1",
+    Folder:"Worlds Hardest Game/1/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"World's Hardest Game 2",
+    Folder:"Worlds Hardest Game/2/",
+    Thumbnail:"thumbnail.jpg"
+  },
+  {
+    Name:"yohoho.io",
+    Thumbnail:"thumbnail.jpg"
+  },
+];
+
+/* "1v1.LOL", "2048", "Aquapark", "Angry Birds", "Bad Piggies", "Basketball Stars",
+    "Bitlife", "Blackjack", "BloonsTD", "Bloxorz", "CookieClicker", "CrossyRoad",
+    "DonkeyKong", "DOOM", "DuckHunt", "Ducklife_1", "Ducklife_2", "Ducklife_3", "ElectricMan 2", "FruitNinja",
+    "FNAF_1", "FNAF_2", "FNAF_3", "FNAF_4", "FNAW", "GoogleSnake", "House of Horers Simulator", "Impossible Quiz", "Jacksmith",
+    "Learn 2 Fly", "Minecraft_1.8.8", "MotoX3M", "MotoX3M 2", "Minesweeper", "Pacman", "PapaPizza", "wolfen",
+    "Pong", "RetroBowl", "Run_1", "Run_3", "Robux Generator Clicker", "RiddleSchool_1", "RiddleSchool_2", "RiddleSchool_3",
+     "Slope_1", "Slope_Ass", "StickmanHook", "SuperMario 63", "SuperMario 64", "SoobwaySurfers", "Tetris", 
+     "Vex_3" ,"Worlds Hardest Game_1", "Worlds Hardest Game_2", "Wordle", "yohoho.io" */
--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 const resultsText = document.getElementById("results");
 const clear = document.getElementById("clear");
-const apps = ["1v1.LOL", "2048", "Aquapark", "Angry Birds", "Bad Piggies", "Basketball Stars",
+/* const apps = ["1v1.LOL", "2048", "Aquapark", "Angry Birds", "Bad Piggies", "Basketball Stars",
     "Bitlife", "Blackjack", "BloonsTD", "Bloxorz", "CookieClicker", "CrossyRoad",
     "DonkeyKong", "DOOM", "DuckHunt", "Ducklife_1", "Ducklife_2", "Ducklife_3", "ElectricMan 2", "FruitNinja",
     "FNAF_1", "FNAF_2", "FNAF_3", "FNAF_4", "FNAW", "GoogleSnake", "House of Horers Simulator", "Impossible Quiz", "Jacksmith",
     "Learn 2 Fly", "Minecraft_1.8.8", "MotoX3M", "MotoX3M 2", "Minesweeper", "Pacman", "PapaPizza", "wolfen",
     "Pong", "RetroBowl", "Run_1", "Run_3", "Robux Generator Clicker", "RiddleSchool_1", "RiddleSchool_2", "RiddleSchool_3",
      "Slope_1", "Slope_Ass", "StickmanHook", "SuperMario 63", "SuperMario 64", "SoobwaySurfers", "Tetris", 
-     "Vex_3" ,"Worlds Hardest Game_1", "Worlds Hardest Game_2", "Wordle", "yohoho.io"];
-apps.sort();
+     "Vex_3" ,"Worlds Hardest Game_1", "Worlds Hardest Game_2", "Wordle", "yohoho.io"]; 
+apps.sort(); */
 
 document.head.appendChild(altCSS);
 
@@ -69,26 +69,32 @@ function openSite(url) {
 
 function searchApp(name) {
   var foundApps = [];
-  var results = 0;
-  // appsDiv.innerHTML = "";
   if (name !== "") {
-    altCSS.innerHTML = `.appsButton { display: none; animation: none; }`;
-    document.querySelectorAll(".appsButton").forEach((app) => {
-      app.style = "";
+    // appsDiv.innerHTML = "";
+    altCSS.innerHTML = `.appsButton { display: none; animation: none; } .foundApp { display: block !important; }`;
+    document.querySelectorAll(".foundApp").forEach((app) => {
+      app.classList.remove("foundApp");
     });
-    apps.forEach(function (app, i) {
-      if (app.toLowerCase().replaceAll("_", " ").includes(name.toLowerCase())) {
+    apps.forEach(function (app) {
+      if (app.Name.toLowerCase().includes(name.toLowerCase())) {
         foundApps.push(app);
-        results++;
       }
     });
     foundApps.sort();
-    foundApps.forEach(createApps);
+    foundApps.forEach(function(app) {
+      if (app.Hidden === true) {
+        return;
+      }
+
+      const button = document.getElementById(`appButton${app.Name}`);
+      button.classList.add("foundApp");
+    });
+
     if (name !== "") {
       if (results == 1) {
-        resultsText.innerHTML = `${results} result for '${name}'`;
+        resultsText.innerHTML = `${foundApps.length} result for '${name}'`;
       } else {
-        resultsText.innerHTML = `${results} results for '${name}'`;
+        resultsText.innerHTML = `${foundApps.length} results for '${name}'`;
       }
     } else {
       resultsText.innerHTML = "";
@@ -100,18 +106,49 @@ function searchApp(name) {
   }
 }
 
-function createApp(name, hint, url) {
+function createApp(name, hint, url, thumbnail, newlyAdded, broken, fixed) {
   if (!document.getElementById(`appButton${name}`)) {
     const b = document.createElement("button");
     const p = document.createElement("p");
     const img = document.createElement("img");
     b.id = `appButton${name}`;
     b.className = "appsButton";
-    p.innerText = name;
     b.title = hint;
     b.onclick = function () {
       openSite(`${url}`);
     };
+
+    p.innerText = name;
+    p.classList.add("appName");
+
+    const tags = document.createElement("div");
+    tags.classList.add("tags");
+    b.appendChild(tags);
+
+    if (newlyAdded) {
+      const newP = document.createElement("p");
+      newP.innerText = "NEW";
+      newP.classList.add("new");
+      newP.title = "This app was recently added (within the last 5 days)";
+      tags.appendChild(newP);
+    }
+
+    if (broken) {
+      const newP = document.createElement("p");
+      newP.innerText = "!";
+      newP.classList.add("broken");
+      newP.title = "This app may not work at the moment, we'll fix it soon.";
+      tags.appendChild(newP);
+    }
+
+    if (fixed) {
+      const newP = document.createElement("p");
+      newP.innerText = "FIXED";
+      newP.classList.add("fixed");
+      newP.title = "This app was recently fixed.";
+      tags.appendChild(newP);
+    }
+
     b.appendChild(p);
     b.appendChild(img);
     document.getElementById("apps").appendChild(b);
@@ -121,28 +158,40 @@ function createApp(name, hint, url) {
     };
     
     img.onerror = function() {
-      if (img.src.includes(".jpg")) {
-        img.src = `${url}/../thumbnail.png`;
-      } else if (img.src.includes(".png")) {
-        img.src = `${url}/../thumbnail.jpeg`;
-      } else if (img.src.includes(".jpeg")) {
-        img.src = `${url}/../thumbnail.webp`;
-      } else {
-        img.src = "https://img.freepik.com/premium-photo/digital-black-green-squares_161488-652.jpg";
-      }
+      img.src = "https://img.freepik.com/premium-photo/digital-black-green-squares_161488-652.jpg";
     };
-    img.src = `${url}/../thumbnail.jpg`;
+    img.src = `${url}/../${thumbnail || "thumbnail.png"}`;
   }
 }
 
-function createApps(app, i) {
+function createApps(app) {
+  if (app.Hidden) {
+    return;
+  } else {
+    app.Hidden = false;
+  }
+
+  app.Name = app.Name || "No Name";
+  app.Folder = app.Folder || app.Name;
+  app.Index = app.Index || "index.html";
+  app.Thumbnail = app.Thumbnail || "";
+  app.Broken = app.Broken || false;
+  app.Fixed = app.Fixed || false;
+  app.Added = app.Added || new Date("January 1, 2020");
+  let newlyAdded = false;
+  let currentDate = new Date();
+  let utc1 = Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+  let utc2 = Date.UTC(app.Added.getFullYear(), app.Added.getMonth(), app.Added.getDate());
+  // Calculate the time difference in milliseconds
+  let timeDiff = Math.abs(utc2 - utc1);
+  // Convert milliseconds to days
+  let daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  if (daysDiff <= 5) {
+    newlyAdded = true;
+  }
+
   if (!document.getElementById(`appButton${app}`)) {
-    if (app.includes("_")) {
-      var a = app.split("_");
-      createApp(a[0] + " " + a[1], "Play " + a[0] + " " + a[1], "https://joebidenrealomg.github.io/da-hub/apps/" + a[0] + "/" + a[1] + "/index.html");
-    } else {
-      createApp(app, "Play " + app, "https://joebidenrealomg.github.io/da-hub/apps/" + app + "/index.html", i);
-    }
+      createApp(app.Name, "Play " + app.Name, "apps/" + app.Folder + "/" + app.Index, app.Thumbnail, newlyAdded, app.Broken, app.Fixed);
   } else {
     document.getElementById(`appButton${app}`).style.animation = "";
     document.getElementById(`appButton${app}`).style.display = "block";
